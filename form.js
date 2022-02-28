32de1c544feacfc02de57e2ee8c1b182e682e43b
TanhyaB <93513266+TanhyaB@users.noreply.github.com>
2022-02-28 19:23:07 +0000

    Update form.js

--- form.js
+++ form.js
@@ -15,15 +15,12 @@ const validateUsername = () => {
     const usernameValue = username.value;
     
     if(usernameValue === ""){ 
-        username.style.border = "2px solid red";
         alert ("Username cannot be blank. It must contain at least 6 characters")
         return false;
     } 
     if (usernameValue.length <6){
-        username.style.border = "1px solid red";
         return false;
     } else {
-        username.style.border = "2px solid green";
         return true;
     }
 }
@@ -33,16 +30,13 @@ const validateUsername = () => {
         const emailValue = email.value;
         const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
-      if(emailValue === "") {
-        email.style.border = "2px solid red";
+      if(emailValue === "")
         alert ("Please enter your email address!")
         return false;
       }
       if(emailValue.match(emailFormat)){
-        email.style.border = "2px solid green";
         return true;
       } else {
-            email.style.border = "2px solid red";
           alert ("You have entered and invalid email address!")
           return false;
       }
@@ -54,15 +48,12 @@ const validateUsername = () => {
       const passwordFormat = /([ A-Za-z])|(\d)|([*.!@#$%^&(){}:;<>,.?~_+-=])/;
 
       if(passwordValue === ""){
-          password.style.border = "2px solid red";
           alert ("Please enter your password");
           return false;
       }
       if(passwordValue.match(passwordFormat)){
-          password.style.border = "2px solid green";
           return true;
       }else {
-          password.style.border = "2px solid red";
           alert ("Your password should contain: uppercase, lowercase, number and special character.")
           return false;
       }
